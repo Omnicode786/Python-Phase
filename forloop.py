8cf5026aacc6cab43ca8b30fc450188314a6ee69
Omnicode786 <muzammil.67520@iqra.edu.pk>
2024-12-26 15:00:23 +0500

    forloop

---
+++ forloop.py
@@ -0,0 +1,7 @@
+# range function starts by 0
+#the list of number generated will be 1 less than the given value similar to how array works
+
+#list using square brackets
+friends = ['Muzammil', 'Taha', 'Suman', 'Minhaj']
+for friend in friends:
+    print("Hey " + friend)
