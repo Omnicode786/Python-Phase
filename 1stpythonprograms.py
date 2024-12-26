7762f65f54d9dcd150bbda887b8aa7bf6290574c
Omnicode786 <muzammil.67520@iqra.edu.pk>
2024-12-26 14:59:19 +0500

    1st

---
+++ 1stpythonprograms.py
@@ -0,0 +1,19 @@
+
+
+
+print("Hello Muzammil The beast")
+
+print(4+5)
+print(9*5)
+print(5-5)
+print(4-5)
+print(1/5)
+print(1/3)
+print(7/6)
+print(2/6)
+# peridoic numbers will show lots of decimal places
+print(((2025/5)-32)-55)
+# in order to take a square of any number we do it like this
+# // basically any power
+print(2**10)
+# here ** simply means exponent
