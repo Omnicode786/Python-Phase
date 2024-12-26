ffe9d0c943615065d010c5feb02997eb3ff2c78f
Omnicode786 <muzammil.67520@iqra.edu.pk>
2024-12-26 14:59:49 +0500

    conditional

---
+++ conditionals.py
@@ -0,0 +1,19 @@
+print(10>1)
+print("cat" == "dog")
+# basically this can vbe done with all we cann ot > < or with int, float to string
+
+# print (1 > "1")
+#the above will give error
+
+print (1 =="1")
+# this will basically see the data type if the data type of the them folllwing are same then true and in this case false
+# // logical operator
+print(25>40 or 1 != 2)
+# the not operator basically how not works in discrete structure
+# not operator makes it so that if a certain condition is true it makes it false and viceversa
+print(not 60>70)
+#the above now becomes true
+
+if 60>40:
+    print("60 is greater than 40")
+
