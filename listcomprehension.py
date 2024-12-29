11d40c8dc9d999b9bfb7a971a0873cd82a67defb
Omnicode786 <muzammil.67520@iqra.edu.pk>
2024-12-30 00:33:18 +0500

    up

--- listcomprehension.py
+++ listcomprehension.py
@@ -20,3 +20,12 @@ print(lengths)
 # for numbers we could possibly right it as
 numbers = [x for x in range(1,101) if x % 3 == 0]
 print(numbers)
+print(sum(numbers))
+def odd_numbers(n):
+	return [x for x in range(1, n+1) if x % 2 != 0]
+
+print(odd_numbers(5))  # Should print [1, 3, 5]
+print(odd_numbers(10)) # Should print [1, 3, 5, 7, 9]
+print(odd_numbers(11)) # Should print [1, 3, 5, 7, 9, 11]
+print(odd_numbers(1))  # Should print [1]
+print(odd_numbers(-1)) # Should print []
